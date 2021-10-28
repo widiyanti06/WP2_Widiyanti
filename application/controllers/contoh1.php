1a9689d84850cd03c93ecc05d0e7597057394dcf
widiyanti06 <iwidiyanti54@gmail.com>
2021-10-28 21:18:43 +0700

    menambahkan isi contoh1.php, config, routes dan controllers

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,1 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Widiyanti
+        Saya tinggal di daerah Purwasari
+        Olahraga yang saya sukai adalah Bulutangkis";
+	}
+}
 
